10fbce80c8a668ea716455201b8666e31344f0bc
Richard Cruz <richard101010cruz@gmail.com>
2020-11-10 00:03:12 -0400

    Se agrego el proyecto con el archivo principal.cpp

---
+++ Project1/Project1/principal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	printf("Nombre:Richard Ausberto Cruz Grimaldez.\nCI: 13217585.\nCU: 35-4880.\nCorreo Electronico: richard101010cruz@gmail.com\n");
+	system("pause");
+	return 0;
+};
