a4ecf56ffa462c444303753923bdf463d6e320aa
Richard Cruz <richard101010cruz@gmail.com>
2020-11-18 13:21:04 -0400

    Cambio de ci

--- Project1/Project1/principal.cpp
+++ Project1/Project1/principal.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-int main () {
-	printf("Nombre:Richard Ausberto Cruz Grimaldez.\nCI: 13217585.\nCU: 35-4880.\nCorreo Electronico: richard101010cruz@gmail.com\n");
+int main() {
+	printf("Nombre:Richard Ausberto Cruz Grimaldez.\nCI: 13217585 CH.\nCU: 35-4880.\nCorreo Electronico: richard101010cruz@gmail.com\n");
 	system("pause");
 	return 0;
 };
