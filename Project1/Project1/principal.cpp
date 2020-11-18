4503f42cb14420da61bb5111410f51e001f54a14
Richard Cruz <richard101010cruz@gmail.com>
2020-11-18 13:23:40 -0400

    Cambioen el CU

--- Project1/Project1/principal.cpp
+++ Project1/Project1/principal.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	printf("Nombre:Richard Ausberto Cruz Grimaldez.\nCI: 13217585 CH.\nCU: 35-4880.\nCorreo Electronico: richard101010cruz@gmail.com\n");
+	printf("Nombre:Richard Ausberto Cruz Grimaldez.\nCI: 13217585 CH.\nCU: 35-4880 Tecnologia-Ingenieria en Sistemas.\nCorreo Electronico: richard101010cruz@gmail.com\n");
 	system("pause");
 	return 0;
 };
